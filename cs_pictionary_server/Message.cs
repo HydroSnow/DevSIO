35b5c1a77339c7916ac7c6d4750ff364b7c9500a
HydroSnow <link9745@gmail.com>
2019-02-18 11:14:16 +0100

    Creating the pictionary server program

---
+++ cs_pictionary_server/Message.cs
@@ -0,0 +1,108 @@
+﻿using System.IO;
+using System.Net.Sockets;
+
+namespace cs_pictionary_server
+{
+    public class Message
+    {
+        public static Message Read(NetworkStream ns)
+        {
+            byte type = (byte)ns.ReadByte();
+
+            MemoryStream ms = new MemoryStream();
+            bool read = true;
+            bool escape = false;
+            while (read)
+            {
+                int b = ns.ReadByte();
+                if (b == -1)
+                {
+                    throw new EndOfStreamException("End of stream reached");
+                }
+                else if (escape)
+                {
+                    if (b == 0)
+                    {
+                        read = false;
+                    }
+                    else if (b == 255)
+                    {
+                        ms.WriteByte(255);
+                    }
+                    else
+                    {
+                        throw new InvalidDataException("Unknown escape code");
+                    }
+                    escape = false;
+                }
+                else
+                {
+                    if (b == 255)
+                    {
+                        escape = true;
+                    }
+                    else
+                    {
+                        ms.WriteByte((byte)b);
+                    }
+                }
+            }
+
+            byte[] data = ms.ToArray();
+            return new Message(type, data);
+        }
+
+        public byte Type
+        {
+            get;
+        }
+
+        public byte[] Data
+        {
+            get;
+        }
+
+        private byte[] bytes;
+
+        public Message(byte type)
+        {
+            Type = type;
+            Data = new byte[0];
+
+            MemoryStream ms = new MemoryStream();
+            ms.WriteByte(Type);
+            ms.WriteByte(255);
+            ms.WriteByte(0);
+            bytes = ms.ToArray();
+        }
+
+        public Message(byte type, byte[] data)
+        {
+            Type = type;
+            Data = data;
+            
+            MemoryStream ms = new MemoryStream();
+            ms.WriteByte(Type);
+            foreach (byte b in Data)
+            {
+                if (b == 255)
+                {
+                    ms.WriteByte(255);
+                }
+                ms.WriteByte(b);
+            }
+            ms.WriteByte(255);
+            ms.WriteByte(0);
+            bytes = ms.ToArray();
+        }
+
+        public void Write(NetworkStream ns)
+        {
+            lock (ns)
+            {
+                ns.Write(bytes, 0, bytes.Length);
+                ns.Flush();
+            }
+        }
+    }
+}
---
+++ cs_pictionary_server/User.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace cs_pictionary_server
+{
+    public class User
+    {
+        private Program program;
+        private Thread t;
+        private readonly Socket cli;
+        private readonly NetworkStream ns;
+
+        public User(Program program, Socket socket)
+        {
+            this.program = program;
+            cli = socket;
+            ns = new NetworkStream(cli);
+
+            Message msg = new Message(4);
+            msg.Write(ns);
+
+            byte[] bytes = new byte[24];
+            Buffer.BlockCopy(BitConverter.GetBytes(0f), 0, bytes, 0, 4);
+            Buffer.BlockCopy(BitConverter.GetBytes(0f), 0, bytes, 4, 4);
+            Buffer.BlockCopy(BitConverter.GetBytes(200f), 0, bytes, 8, 4);
+            Buffer.BlockCopy(BitConverter.GetBytes(200f), 0, bytes, 12, 4);
+            Buffer.BlockCopy(BitConverter.GetBytes(0), 0, bytes, 16, 3);
+            Buffer.BlockCopy(BitConverter.GetBytes(30f), 0, bytes, 20, 4);
+
+            Message msg2 = new Message(2, bytes);
+            msg2.Write(ns);
+
+            t = new Thread(ReadThread);
+            t.Start();
+        }
+
+        public void Remove()
+        {
+            t.Abort();
+            try
+            {
+                cli.Close();
+            }
+            catch (Exception)
+            { }
+        }
+
+        public void SendMessage(Message msg)
+        {
+            msg.Write(ns);
+        }        
+
+        private void ReadThread()
+        {
+            try
+            {
+                while (true)
+                {
+                    Message msg = Message.Read(ns);
+
+                    switch (msg.Type)
+                    {
+                        case 1:
+                            program.BroadcastMessage(msg);
+                            break;
+
+                        case 2:
+                        case 3:
+                            program.BroadcastMessage(msg, this);
+                            break;
+
+                        default:
+                            throw new InvalidDataException("Unknown type");
+                    }
+                }
+            }
+            catch (ThreadAbortException)
+            { }
+            catch (Exception)
+            {
+                try
+                {
+                    cli.Close();
+                }
+                catch (Exception)
+                { }
+
+                program.RemoveUser(this);
+            }
+        }
+    }
+}
