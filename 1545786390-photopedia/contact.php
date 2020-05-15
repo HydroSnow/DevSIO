23e9d3cd8acb392aedd3a910cb07749c09b09c22
Alexis Trupin <link9745@gmail.com>
2020-05-15 22:55:17 +0200

    adding photopedia

---
+++ 1545786390-photopedia/contact.php
@@ -0,0 +1,40 @@
+<?php 
+	require('template.php'); 
+	show_up_header();
+?>
+<link rel="stylesheet" type="text/css" href="main.css">
+<link rel="stylesheet" type="text/css" href="navbar.css">
+<?php
+	show_low_header();
+?>
+
+<h2>Contact</h2>	  
+<a href="#">
+	<div class="Dylan" align="center">
+		<img src="icone utilisateur.png" alt="photo1" />
+		<p>Dylan</p>
+	</div>
+</a>
+<a href="#">
+	<div class="Mathis" align="center">
+		<img src="icone utilisateur.png" alt="photo1">
+		<p>Mathis</p>
+	</div>
+</a>
+<a href="#">
+	<div class="Alexis" align="center">
+		<img src="icone utilisateur.png" alt="photo1"> 
+		<p>Alexis</p>
+	</div>
+</a>
+<p>Numéro de téléphone : 06 78 98 51 23</p>
+<p>Localisation : Laval</p>
+<p>Code postal : 53000</p>
+<fieldset>
+	<legend>Nos coordonnées</legend>
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2665.162008153737!2d-0.7675176840594333!3d48.08781597921948!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48090210090c0f27%3A0x7c8fecbe9eef2e52!2sL&#39;Aquabulle!5e0!3m2!1sfr!2sfr!4v1538743730722" frameborder="0" style="border:0"></iframe>
+</fieldset>
+	
+<?php 
+	show_footer();
+?>
